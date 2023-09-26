4399d8371d1973a21a67a8b6502e3c3e91944607
modengann <99417989+modengann@users.noreply.github.com>
2023-09-25 20:52:32 -0400

    init

---
+++ src/App.java
@@ -0,0 +1,21 @@
+import processing.core.*;
+
+public class App extends PApplet{
+
+    public static void main(String[] args){
+        PApplet.main("App");
+    }
+    
+    public void setup(){
+        
+    }
+
+    public void settings(){
+        
+    }
+
+    public void draw(){
+       
+
+    }
+}
